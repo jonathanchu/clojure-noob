f7847c15bbf5f1d4e96c820125973888281826de
Jonathan Chu <jonathan.chu@me.com>
2015-06-18 00:22:45 -0400

    add more clojure fun aka testing

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,3 +5,22 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "I'm a little teapot!"))
+(println "Cleanliness is next to godliness")
+
+(defn train
+  []
+  (println "Choo choo!"))
+
+(map)
+
+(+ 1 (* 2 3) 4)
+(+ 1 (* 2 3) 4)
+
+(map (comp record first)
+     (d/q '[:find ?post
+            :in $ ?search
+            :wohere
+            [(fulltext $ :post/content ?search)
+             [[?post ?content]]]]
+          (db/db)
+          (:q params)))
